0bbc9c37b0a664f5cc4753d6d96ff86dbaebf0c4
Leo Nguyen <nguyenchihuy10@gmail.com>
2024-09-03 14:50:56 -0400

    Fix issue with deployment funds - hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,10 @@
-require('@nomicfoundation/hardhat-toolbox');
-require('dotenv').config();
-
-const { POLYGON_MAINNET_URL, PRIVATE_KEY } = process.env;
+require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
   solidity: "0.8.24",
   networks: {
-    polygon: {
-      url: POLYGON_MAINNET_URL || '',
-      accounts: PRIVATE_KEY ? [`0x${PRIVATE_KEY}`] : [],
-    },
+    hardhat: {
+      chainId: 1337
+    }
   },
 };
